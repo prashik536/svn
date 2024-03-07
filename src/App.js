45455e64cfc57075082b86660e088c4443afcedb
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2024-03-07 19:19:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const callhello = () => {
+    axios.get('http://localhost:5000')
+      .then(response => {
+        alert(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button onClick={callhello}>Click</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
